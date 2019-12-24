869199b1e5a19e1ba0938cc1ffc283fdb9432300
Irving Alain Aguilar Pérez <a01703171@itesm.mx>
2019-12-24 15:05:54 -0600

    Update: Added route model binding

--- dmh/app/Product.php
+++ dmh/app/Product.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     public function getRouteKeyName(){
-        return 'title';
+        return 'url';
     }
 }
--- dmh/resources/views/products/index.blade.php
+++ dmh/resources/views/products/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 @section('title', 'Products')
 @section('content')
-    <h1>Products</h1>
+    <h1>Productos</h1>
     <ul>
         @forelse ($products as $product)
             <li><a href="{{ route('products.show', $product) }}">{{ $product->title }}</a></li>
